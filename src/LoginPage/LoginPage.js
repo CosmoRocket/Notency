6f7e7dcea5088f4fa0de93ffe354fe8974c06dc4
johnvoon <johnksvoon@gmail.com>
2018-01-12 15:46:12 +1100

    Change LoginPage

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -3,23 +3,15 @@ import Input from '../components/Input'
 import LoginButton from '../components/LoginButton'
 import Checkbox from '../components/Checkbox'
 import './LoginPage.css'
-import './embassy_english.png'
+import logo from './embassy_english.png'
 
-function LoginPage({
-  onSubmit,
-  passwordLink
-}) {
+function LoginPage({ onSubmit, passwordLink }) {
   return (
-    <Fragment>
-      <img src="./embassy_english.png" alt="embassy english logo" />
+    <div>
+      <img src={logo} alt="embassy english logo" />
       <h2>Welcome Back!</h2>
       <form onSubmit={onSubmit}>
-        <Input
-          type="name"
-          name="username"
-          placeholder="User"
-          iconName="user"
-        />
+        <Input type="name" name="username" placeholder="User" iconName="user" />
         <Input
           type="password"
           name="password"
@@ -27,17 +19,15 @@ function LoginPage({
           iconName="lock"
         />
         <LoginButton />
-        <div className='d-flex justify-content-between'>
-          <Checkbox
-            name="rememberMe"
-            value="rememberMe"
-            text="Rember me"
-          />
-          <a href={passwordLink} className='password-link'>Forgot your password?</a>
+        <div className="d-flex justify-content-between">
+          <Checkbox name="rememberMe" value="rememberMe" text="Remember me" />
+          <a href={passwordLink} className="password-link">
+            Forgot your password?
+          </a>
         </div>
       </form>
       <p>An app for sending bulk SMS and email notifications</p>
-    </Fragment>
+    </div>
   )
 }
 
