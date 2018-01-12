6275d3727c9b9171440144af7ad847140cd031ef
johnvoon <johnksvoon@gmail.com>
2018-01-12 15:08:32 +1100

    Add LoginButton component

---
+++ src/components/LoginButton/LoginButton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './LoginButton.css'
+
+const LoginButton = () => {
+  return (
+    <Button
+      type="submit"
+      text="Login"
+    />
+  )
+}
+
+export default LoginButton
