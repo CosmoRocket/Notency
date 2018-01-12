602215d04fa3ed47a714610a123b9693de50dd54
johnvoon <johnksvoon@gmail.com>
2018-01-12 15:10:29 +1100

    Add TabbedNav component

---
+++ src/components/TabbedNav/TabbedNav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function TabbedNav({ handleChangeActiveTab, activeTab, tabs }) {
+  const navTabs = tabs.map((TabContent, index) => {
+    return (
+      <li class="nav-item" onClick={() => handleChangeActiveTab(index)}>
+        <a class={`nav-link ${activeTab === index && 'active'}`} href="#">
+          <TabContent />
+        </a>
+      </li>
+    )
+  })
+  return (
+    <nav>
+      <ul class="nav nav-tabs d-none d-sm-flex">{navTabs}</ul>
+    </nav>
+  )
+}
