2578bf91ea7ada717d37a4a03344236f1158533f
johnvoon <johnksvoon@gmail.com>
2018-01-12 15:11:02 +1100

    Add Checkbox component

---
+++ src/components/Checkbox/Checkbox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Checkbox = () => {
+  return (
+    <div className="form-check">
+      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+      <label className="form-check-label" htmlFor="exampleCheck1">
+        Check me out
+      </label>
+    </div>
+  )
+}
+
+export default Checkbox
