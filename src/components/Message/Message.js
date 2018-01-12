8a8185d0223da7acf2cb42156d6a95c34eb57fb2
johnvoon <johnksvoon@gmail.com>
2018-01-12 15:08:10 +1100

    Add Message component

---
+++ src/components/Message/Message.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Icon } from 'react-fa'
+import './Message.css'
+
+export default function Message({
+  recipientId,
+  contactNumber,
+  recipientName,
+  messageBody
+}) {
+  return (
+    <div className="Message py-1">
+      <div className="d-flex justify-content-between">
+        <small className="m-0">ID: {recipientId}</small>
+        <small>{contactNumber}</small>
+      </div>
+      <p className="m-0">
+        <span className="font-weight-bold">{recipientName}</span> wrote:{' '}
+        <span className="font-italic">{messageBody}</span>
+      </p>
+    </div>
+  )
+}
