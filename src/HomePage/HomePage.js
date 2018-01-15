293fae4d9943a9e3e64e69fce189a9cc76a7ba7d
johnvoon <johnksvoon@gmail.com>
2018-01-15 14:37:58 +1100

    Remove activeTab state from HomePage component

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -10,66 +10,53 @@ import Checkbox from '../components/Checkbox'
 import ContentContainer from '../components/ContentContainer'
 import { Link } from 'react-router-dom'
 
-class HomePage extends React.Component {
-  state = {
-    activeTab: 0
-  }
+const HomePage = ({
+  notifications,
+  announcements,
+  handleChangeActiveTab,
+  activeTab
+}) => {
+  const notificationsList = notifications.map(notification => {
+    return <Notification {...notification} responses="10/60" />
+  })
+  const announcementsList = announcements.map(announcement => {
+    return <Announcement {...announcement} />
+  })
+  return (
+    <div>
+      <MobileNav />
+      <div className="d-flex">
+        <DesktopNav />
+        <ContentContainer>
+          <TabbedNav
+            activeTab={activeTab}
+            tabs={[
+              () => <p className="m-0">Emergency Notifications</p>,
+              () => <p className="m-0">General Announcements</p>
+            ]}
+            handleChangeActiveTab={handleChangeActiveTab}
+          />
+          {activeTab === 0 ? (
+            <Link
+              to="/notifications/new"
+              className="btn btn-danger text-uppercase font-weight-bold my-2"
+            >
+              New Notification
+            </Link>
+          ) : (
+            <Link
+              to="/announcements/new"
+              className="btn btn-danger text-uppercase font-weight-bold my-2"
+            >
+              New Announcement
+            </Link>
+          )}
 
-  handleChangeActiveTab = index => {
-    this.setState({ activeTab: index })
-  }
-
-  render() {
-    const { notifications, announcements } = this.props
-    const { activeTab } = this.state
-    const notificationsList = notifications.map(notification => {
-      return <Notification {...notification} responses="10/60" />
-    })
-    const announcementsList = announcements.map(announcement => {
-      return <Announcement {...announcement} />
-    })
-    return (
-      <div>
-        <MobileNav />
-        <div className="d-flex">
-          <DesktopNav />
-          <ContentContainer>
-            <TabbedNav
-              activeTab={activeTab}
-              tabs={[
-                () => <p className="m-0">Emergency Notifications</p>,
-                () => <p className="m-0">General Announcements</p>
-              ]}
-              handleChangeActiveTab={this.handleChangeActiveTab}
-            />
-            {this.state.activeTab === 0 ? (
-              <Link
-                to="/notifications/new"
-                className="btn btn-danger text-uppercase font-weight-bold my-2"
-              >
-                New Notification
-              </Link>
-            ) : (
-              <Link
-                to="/announcements/new"
-                className="btn btn-danger text-uppercase font-weight-bold my-2"
-              >
-                New Announcement
-              </Link>
-            )}
-
-            {this.state.activeTab === 0 ? notificationsList : announcementsList}
-            <Message
-              recipientId="123891273"
-              contactNumber="0476143646"
-              recipientName="John Voon"
-              messageBody="OK"
-            />
-          </ContentContainer>
-        </div>
+          {activeTab === 0 ? notificationsList : announcementsList}
+        </ContentContainer>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default HomePage
