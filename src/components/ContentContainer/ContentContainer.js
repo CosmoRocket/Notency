68cfa0309de6057f00591e3b749555cc3ca17911
johnvoon <johnksvoon@gmail.com>
2018-01-15 11:46:10 +1100

    Add ContentContainer component

---
+++ src/components/ContentContainer/ContentContainer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './ContentContainer.css'
+
+const ContentContainer = ({ children }) => {
+  return <div className="ContentContainer">{children}</div>
+}
+
+export default ContentContainer
