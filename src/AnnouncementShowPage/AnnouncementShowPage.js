010f8916036a95aeda06be440f7d4fd8dc7e4b43
johnvoon <johnksvoon@gmail.com>
2018-01-15 14:37:10 +1100

    Add AnnouncementShowPage

--- src/AnnouncementShowPage/AnnouncementShowPage.js
+++ src/AnnouncementShowPage/AnnouncementShowPage.js
@@ -3,21 +3,44 @@ import ContentContainer from '../components/ContentContainer'
 import MobileNav from '../components/MobileNav'
 import DesktopNav from '../components/DesktopNav'
 import TabbedNav from '../components/TabbedNav'
+import Message from '../components/Message'
+import moment from 'moment'
+import { Link } from 'react-router-dom'
 
-class NotificationShowPage extends Component {
-  state = {}
+class AnnouncementShowPage extends Component {
+  state = {
+    currentAnnouncement: {}
+  }
+
+  componentDidMount() {
+    const { announcements, location } = this.props
+    const currentAnnouncement = announcements.find(announcement => {
+      return announcement.id === location.pathname.split('/').slice(-1)[0]
+    })
+
+    this.setState({ currentAnnouncement })
+  }
+
   render() {
-    const { activeTab } = this.props
+    const { currentAnnouncement } = this.state
+
     return (
       <div>
         <MobileNav />
         <div className="d-flex">
           <DesktopNav />
-          <ContentContainer />
+          <ContentContainer>
+            <p className="text-right">
+              {moment(currentAnnouncement.sentAt).format('D MMM YYYY')}
+            </p>
+            <h2 className="text-center">{currentAnnouncement.title}</h2>
+            <p>{currentAnnouncement.body}</p>
+            <Link to="/">Back</Link>
+          </ContentContainer>
         </div>
       </div>
     )
   }
 }
 
-export default NotificationShowPage
+export default AnnouncementShowPage
