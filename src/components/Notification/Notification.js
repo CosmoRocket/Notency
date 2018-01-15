852a10d8b69c4c99dfa7dfc87435129206c77a0f
johnvoon <johnksvoon@gmail.com>
2018-01-15 12:38:28 +1100

    Add moment.js for formatting dates

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Icon } from 'react-fa'
 import './Notification.css'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
 
 export default function Notification({ id, title, body, sentAt, responses }) {
   return (
     <div className="Notification py-1">
       <Link to={`/notifications/${id}`}>
         <div className="d-flex justify-content-between">
-          <small className="m-0">{sentAt}</small>
+          <small className="m-0">{moment(sentAt).format('D MMM YYYY')}</small>
           {responses && (
             <small>
               <Icon className="text-success mr-2" name="mail-reply" />
