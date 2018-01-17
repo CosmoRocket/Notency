28af6082fa82b5c96bc06147ca9506e69448acaf
Alex-Farries <adfarries@gmail.com>
2018-01-17 14:40:11 +1100

    Add form libraries with John's changes to AnnouncementForm

--- src/components/AnnouncementForm/AnnouncementForm.js
+++ src/components/AnnouncementForm/AnnouncementForm.js
@@ -4,8 +4,15 @@ import { Editor } from 'react-draft-wysiwyg'
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 import Button from '../../components/Button'
 import Input from '../../components/Input'
+import TextArea from '../../components/TextArea'
+import RadioMenu from '../../components/RadioMenu'
 import draftToHtml from 'draftjs-to-html'
 import { Link } from 'react-router-dom'
+import ReactSelect from 'react-select'
+import { Formik } from 'formik'
+import Yup from 'yup'
+import { groupBy } from 'ramda'
+import capitalize from 'lodash/capitalize'
 
 export default function AnnouncementForm({
   subject,
@@ -13,61 +20,117 @@ export default function AnnouncementForm({
   contentState
 }) {
   return (
-    <form
-      onSubmit={event => {
-        event.preventDefault()
-
-        const subject = event.target.elements.subject.value
-        const contentData = draftToHtml(contentState)
-
-        const announcement = {
-          subject: subject,
-          contentData: contentData
-        }
-
-        this.handleCreateAnnouncement(announcement)
+    <Formik
+      initialValues={{
+        subject: '',
+        body: '',
+        group: 'all',
+        nationality: []
       }}
-    >
-      {/* RADIO BUTTONS - select all or by group */}
-      <div className="radioMenu">
-        <p>To: </p>
-        <input type="radio" id="allChoice" name="groupSelect" value="all" />
-        <label htmlFor="allChoice">All</label>
-        <input
-          type="radio"
-          id="nationalityChoice"
-          name="groupSelect"
-          value="nationality"
-        />
-        <label htmlFor="nationalityChoice">Nationality</label>
-        <input type="radio" id="roleChoice" name="groupSelect" value="role" />
-        <label htmlFor="roleChoice">Role</label>
-        <input
-          type="radio"
-          id="gradDateChoice"
-          name="groupSelect"
-          value="gradDate"
-        />
-        <label htmlFor="gradDateChoice">Grad Date</label>
-      </div>
-      {/* INPUT FOR EMAIL SUBJECT */}
-      <Input type="subject" name="subject" placeholder="Subject" />
-      {/* REACT-EDITOR COMPONENT (Pass options in as props) */}
-      <Editor
-        wrapperClassName="editorSection"
-        editorClassName="wrapperSection"
-        handleContentStateChange={handleContentStateChange}
-        toolbar={{
-          options: ['inline', 'blockType', 'fontSize', 'fontFamily']
+      validationSchema={Yup.object().shape({
+        subject: Yup.string().required('Please enter a subject'),
+        body: Yup.string().required('Please enter a message')
+      })}
+      render={({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        setFieldValue
+      }) => {
+        const arrangeByNationality = groupBy(recipient => recipient.nationality)
+        const nationalities =
+          recipients &&
+          Object.keys(arrangeByNationality(recipients)).map(nationality => {
+            return { value: nationality, label: capitalize(nationality) }
+          })
+  
+          return (
+            <form
+              onSubmit={event => {
+                event.preventDefault()
+        
+                const subject = event.target.elements.subject.value
+                const contentData = draftToHtml(contentState)
+        
+                const announcement = {
+                  subject: subject,
+                  contentData: contentData
+                }
+        
+                this.handleCreateAnnouncement(announcement)
+              }}
+            >
+              {/* RADIO BUTTONS - select all or by group */}
+              <div className="d-flex">
+                <p className="m-0">To: </p>
+                <RadioMenu
+                  name="group"
+                  options={['All', 'Nationality', 'Role']}
+                  onChange={handleChange}
+                />
+              </div>
+              {values.group === 'nationality' && (
+                <ReactSelect
+                  name="nationality"
+                  placeholder="Select a nationality"
+                  onChange={selectedValue =>
+                    setFieldValue('nationality', selectedValue)
+                  }
+                  value={values.nationality}
+                  multi={true}
+                  options={nationalities}
+                />
+              )}
+              {values.group === 'role' && (
+                <ReactSelect
+                  name="role"
+                  placeholder="Select a role"
+                  onChange={selectedValue => setFieldValue('role', selectedValue)}
+                  value={values.role}
+                  options={[
+                    { value: 'teacher', label: 'Teacher' },
+                    { value: 'student', label: 'Student' }
+                  ]}
+                />
+              )}
+              {/* INPUT FOR EMAIL SUBJECT */}
+              <Input
+                name="subject"
+                placeholder="Subject"
+                value={values.subject}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                errorMessage={errors.subject && touched.subject && errors.subject}
+              />
+              {/* REACT CONTENT EDITOR (pass in options as props) */}
+              <Editor
+                wrapperClassName="editorSection"
+                editorClassName="wrapperSection"
+                handleContentStateChange={handleContentStateChange}
+                toolbar={{
+                  options: ['inline', 'blockType', 'fontSize', 'fontFamily']
+                }}
+              />
+              {/* BOTTOM BUTTONS */}
+              <div className="formActions">
+                <Link className="formBack" to="/">
+                  Back
+                </Link>
+                <p
+                  className={`char-length ${values.body.length === 160 &&
+                    'char-limit-reached'}`}
+                >
+                  Characters used: {values.body.length}/160
+                </p>
+                <Button className="sendButton" text="SEND" />
+              </div>
+            </form>
+          )
         }}
       />
-      {/* BOTTOM BUTTONS */}
-      <div className="formActions">
-        <Link className="formBack" to="/">
-          Back
-        </Link>
-        <Button className="sendButton" text="SEND" />
-      </div>
-    </form>
-  )
-}
+    )
+  }
