3d8bac2c02d6b366b21742fe2c8d3716e6a7590a
johnvoon <johnksvoon@gmail.com>
2018-01-17 20:55:09 +1100

    Prepare Notification request to be submitted to server

--- src/components/NotificationForm/NotificationForm.js
+++ src/components/NotificationForm/NotificationForm.js
@@ -11,20 +11,40 @@ import { Formik } from 'formik'
 import Yup from 'yup'
 import { groupBy } from 'ramda'
 import capitalize from 'lodash/capitalize'
+import isEmpty from 'lodash/isEmpty'
+import { createNotification } from '../../api/notifications'
+import { reject } from 'ramda'
 
 export default function NotificationForm({ recipients }) {
   return (
     <Formik
       initialValues={{
         subject: '',
+        code: '',
         body: '',
+        bodyHtml: '',
         group: 'all',
-        nationality: []
+        recipients: recipients,
+        nationality: [],
+        role: undefined,
+        groups: []
       }}
       validationSchema={Yup.object().shape({
+        code: Yup.string().required('Please enter a 3-digit code'),
         subject: Yup.string().required('Please enter a subject'),
         body: Yup.string().required('Please enter a message')
       })}
+      onSubmit={(values, { setSubmitting, setErrors }) => {
+        createNotification(values).then(
+          notification => {
+            setSubmitting(false)
+            return notification
+          },
+          errors => {
+            setSubmitting(false)
+          }
+        )
+      }}
       render={({
         values,
         errors,
@@ -35,6 +55,19 @@ export default function NotificationForm({ recipients }) {
         isSubmitting,
         setFieldValue
       }) => {
+        const filterRecipients = (allRecipients, category, groups) => {
+          const filteredRecipients = allRecipients.filter(recipient => {
+            if (groups.length === 0) {
+              return true
+            } else {
+              return groups.some(
+                group => recipient[category] === group.value.item
+              )
+            }
+          })
+
+          setFieldValue('recipients', filteredRecipients)
+        }
         const arrangeByNationality = groupBy(recipient => recipient.nationality)
         const nationalities =
           recipients &&
@@ -46,26 +79,40 @@ export default function NotificationForm({ recipients }) {
           })
 
         return (
-          <form
-            onSubmit={event => {
-              event.preventDefault()
-            }}
-          >
+          <form onSubmit={handleSubmit}>
             <div className="d-flex">
               <p className="m-0">To: </p>
               <RadioMenu
                 name="group"
                 options={['All', 'Nationality', 'Role']}
-                onChange={handleChange}
+                selectedValue={values.group}
+                onChange={e => {
+                  handleChange(e)
+                  e.target.value === 'all' &&
+                    setFieldValue('groups', []) &&
+                    filterRecipients(recipients, 'all', [])
+                  e.target.value === 'nationality' &&
+                    setFieldValue('groups', values.nationality) &&
+                    filterRecipients(
+                      recipients,
+                      'nationality',
+                      values.nationality
+                    )
+                  e.target.value === 'role' &&
+                    setFieldValue('groups', values.role ? [] : [values.role]) &&
+                    filterRecipients(recipients, 'role', [values.role])
+                }}
               />
             </div>
             {values.group === 'nationality' && (
               <ReactSelect
                 name="nationality"
                 placeholder="Select a nationality"
-                onChange={selectedValue =>
+                onChange={selectedValue => {
                   setFieldValue('nationality', selectedValue)
-                }
+                  setFieldValue('groups', selectedValue)
+                  filterRecipients(recipients, 'nationality', selectedValue)
+                }}
                 value={values.nationality}
                 multi={true}
                 options={nationalities}
@@ -75,7 +122,11 @@ export default function NotificationForm({ recipients }) {
               <ReactSelect
                 name="role"
                 placeholder="Select a role"
-                onChange={selectedValue => setFieldValue('role', selectedValue)}
+                onChange={selectedValue => {
+                  setFieldValue('role', selectedValue)
+                  setFieldValue('groups', [selectedValue])
+                  filterRecipients(recipients, 'role', [selectedValue])
+                }}
                 value={values.role}
                 options={[
                   {
@@ -86,6 +137,20 @@ export default function NotificationForm({ recipients }) {
                 ]}
               />
             )}
+            <Input
+              name="code"
+              placeholder="Enter a 3-digit code"
+              value={values.code}
+              onChange={e => {
+                handleChange(e)
+                setFieldValue(
+                  'body',
+                  `Reply with '${e.target.value} OK' if you are safe.`
+                )
+              }}
+              onBlur={handleBlur}
+              errorMessage={errors.code && touched.code && errors.code}
+            />
             <Input
               name="subject"
               placeholder="Subject"
@@ -101,7 +166,16 @@ export default function NotificationForm({ recipients }) {
               maxLength="160"
               placeholder="Type your message here..."
               value={values.body}
-              onChange={handleChange}
+              onChange={e => {
+                handleChange(e)
+                setFieldValue(
+                  'bodyHtml',
+                  `<p>${reject(
+                    el => el === '',
+                    e.target.value.split('\n')
+                  ).join('</p><p>')}</p>`
+                )
+              }}
               onBlur={handleBlur}
               errorMessage={errors.body && touched.body && errors.body}
             />
