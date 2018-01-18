02c674d1f5240c378bed4c57e924e403b3056364
johnvoon <johnksvoon@gmail.com>
2018-01-18 16:16:51 +1100

    Add loading screen before data is loaded

--- src/AnnouncementShowPage/AnnouncementShowPage.js
+++ src/AnnouncementShowPage/AnnouncementShowPage.js
@@ -19,35 +19,41 @@ class AnnouncementShowPage extends Component {
 
   render() {
     const { currentAnnouncement } = this.state
-    const capitalize = string =>
-      string.charAt(0).toUpperCase() + string.slice(1)
-    const categories =
-      currentAnnouncement.groups &&
-      groupBy(group => group.name)(currentAnnouncement.groups)
-    const groupElements =
-      currentAnnouncement.groups &&
-      Object.keys(categories).map(category => (
-        <p>
-          {capitalize(category)}:{' '}
-          {categories[category].map(group => group.selected).join(', ')}
-        </p>
-      ))
+    if (!!currentAnnouncement) {
+      const capitalize = string =>
+        string.charAt(0).toUpperCase() + string.slice(1)
+      const categories =
+        currentAnnouncement.groups &&
+        groupBy(group => group.name)(currentAnnouncement.groups)
+      const groupElements =
+        currentAnnouncement.groups &&
+        Object.keys(categories).map(category => (
+          <p>
+            {capitalize(category)}:{' '}
+            {categories[category].map(group => group.selected).join(', ')}
+          </p>
+        ))
 
-    return (
-      <Fragment>
-        <p className="text-right">
-          {moment(currentAnnouncement.createdAt).format('D MMM YYYY')}
-        </p>
-        {
-          groupElements === '' ? <p>Groups Sent To: {groupElements}</p> : <p>Sent to All</p>
-        }
-        <h2 className="text-center">{currentAnnouncement.subject}</h2>
-        <div
-          dangerouslySetInnerHTML={{ __html: currentAnnouncement.bodyHtml }}
-        />
-        <Link to="/">Back</Link>
-      </Fragment>
-    )
+      return (
+        <Fragment>
+          <p className="text-right">
+            {moment(currentAnnouncement.createdAt).format('D MMM YYYY')}
+          </p>
+          {groupElements === '' ? (
+            <p>Groups Sent To: {groupElements}</p>
+          ) : (
+            <p>Sent to All</p>
+          )}
+          <h2 className="text-center">{currentAnnouncement.subject}</h2>
+          <div
+            dangerouslySetInnerHTML={{ __html: currentAnnouncement.bodyHtml }}
+          />
+          <Link to="/">Back</Link>
+        </Fragment>
+      )
+    } else {
+      return <div>Loading</div>
+    }
   }
 }
 
--- src/components/AnnouncementForm/AnnouncementForm.js
+++ src/components/AnnouncementForm/AnnouncementForm.js
@@ -19,186 +19,205 @@ export default function AnnouncementForm({
   recipients,
   handleCreateAnnouncement
 }) {
-  return (
-    <Formik
-      initialValues={{
-        subject: '',
-        recipients: recipients,
-        bodyHtml: '',
-        group: 'all',
-        nationality: [],
-        graduationDate: {},
-        role: undefined, // set to undefined so that placeholder will show up
-        groups: []
-      }}
-      onSubmit={(values, { setSubmitting, setErrors }) => {
-        handleCreateAnnouncement({
-          subject: values.subject,
-          bodyHtml: values.bodyHtml,
-          groups: values.groups.map(group => group.value),
-          recipients: values.recipients
-        })
-      }}
-      validationSchema={Yup.object().shape({
-        subject: Yup.string().required('Please enter a subject'),
-        body: Yup.string().required('Please enter a message')
-      })}
-      render={({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        setFieldValue
-      }) => {
-        const filterRecipients = (allRecipients, category, groups) => {
-          const filteredRecipients = allRecipients.filter(recipient => {
-            if (groups.length === 0 || category === 'all') {
-              return true
-            } else {
-              return groups.some(
-                group => recipient[category] === group.value.item
-              )
-            }
+  if (recipients.length !== 0) {
+    return (
+      <Formik
+        initialValues={{
+          subject: '',
+          recipients: recipients,
+          bodyHtml: '',
+          group: 'all',
+          nationality: [],
+          graduationDate: {},
+          role: undefined, // set to undefined so that placeholder will show up
+          groups: []
+        }}
+        onSubmit={(values, { setSubmitting, setErrors }) => {
+          handleCreateAnnouncement({
+            subject: values.subject,
+            bodyHtml: values.bodyHtml,
+            groups: values.groups.map(group => group.value),
+            recipients: values.recipients
           })
+        }}
+        validationSchema={Yup.object().shape({
+          subject: Yup.string().required('Please enter a subject'),
+          body: Yup.string().required('Please enter a message')
+        })}
+        render={({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          setFieldValue
+        }) => {
+          const filterRecipients = (allRecipients, category, groups) => {
+            const filteredRecipients = allRecipients.filter(recipient => {
+              if (groups.length === 0 || category === 'all') {
+                return true
+              } else if (category === 'graduationDate') {
+                return groups.some(
+                  group =>
+                    moment
+                      .utc(recipient['graduationDate'])
+                      .format('DD/MM/YYYY') === group.value.item
+                )
+              } else {
+                return groups.some(
+                  group => recipient[category] === group.value.item
+                )
+              }
+            })
 
-          setFieldValue('recipients', filteredRecipients)
-        }
-        const arrangeByNationality = groupBy(recipient => recipient.nationality)
-        const nationalities =
-          recipients &&
-          Object.keys(arrangeByNationality(recipients)).map(nationality => {
-            return {
-              value: { name: 'nationality', item: nationality },
-              label: capitalize(nationality)
-            }
-          })
+            setFieldValue('recipients', filteredRecipients)
+          }
+          const arrangeByNationality = groupBy(
+            recipient => recipient.nationality
+          )
+          const nationalities =
+            recipients &&
+            Object.keys(arrangeByNationality(recipients)).map(nationality => {
+              return {
+                value: { name: 'nationality', item: nationality },
+                label: capitalize(nationality)
+              }
+            })
 
-        return (
-          <form onSubmit={handleSubmit}>
-            <div className="d-flex">
-              <p className="m-0">To: </p>
-              <RadioMenu
-                name="group"
-                options={['All', 'Nationality', 'Role', 'Graduation Date']}
-                selectedValue={values.group}
-                onChange={e => {
-                  handleChange(e)
-                  if (e.target.value === 'all') {
-                    const groups = []
-                    setFieldValue('groups', groups)
-                    filterRecipients(recipients, 'all', groups)
-                  } else if (e.target.value === 'nationality') {
-                    const groups = values.nationality
-                    setFieldValue('groups', groups)
-                    filterRecipients(recipients, 'nationality', groups)
-                  } else if (e.target.value === 'role') {
-                    const groups = values.role ? [values.role] : []
-                    setFieldValue('groups', groups)
-                    filterRecipients(recipients, 'role', groups)
-                  } else if (e.target.value === 'graduationDate') {
-                    const groups = isEmpty(values.graduationDate)
-                      ? []
-                      : [values.graduationDate]
-                    setFieldValue('groups', groups)
-                    filterRecipients(recipients, 'graduationDate', groups)
-                  }
-                }}
-              />
-            </div>
-            {values.group === 'nationality' && (
-              <ReactSelect
-                name="nationality"
-                placeholder="Select a nationality"
-                onChange={selectedValue => {
-                  setFieldValue('nationality', selectedValue)
-                  setFieldValue('groups', selectedValue)
-                  filterRecipients(recipients, 'nationality', selectedValue)
-                }}
-                value={values.nationality}
-                multi={true}
-                options={nationalities}
-              />
-            )}
-            {values.group === 'role' && (
-              <ReactSelect
-                name="role"
-                placeholder="Select a role"
-                onChange={selectedValue => {
-                  if (selectedValue === null) {
-                    selectedValue = undefined
-                    setFieldValue('role', selectedValue)
-                    setFieldValue('groups', [])
-                    filterRecipients(recipients, 'role', [])
-                  } else {
-                    setFieldValue('role', selectedValue)
-                    setFieldValue('groups', [selectedValue])
-                    filterRecipients(recipients, 'role', [selectedValue])
-                  }
-                }}
-                value={values.role}
-                options={[
-                  {
-                    value: { name: 'role', item: 'staff' },
-                    label: 'Staff'
-                  },
-                  { value: { name: 'role', item: 'student' }, label: 'Student' }
-                ]}
-              />
-            )}
-            {values.group === 'graduationDate' && (
-              <Input
-                name="graduationDate"
-                type="date"
-                value={values.graduationDate.label}
-                onChange={e => {
-                  const group = {
-                    value: {
-                      name: 'graduationDate',
-                      item: moment(e.target.value, 'YYYY-MM-DD').format(
-                        'DD/MM/YYYY'
-                      )
+          return (
+            <form onSubmit={handleSubmit}>
+              <div className="d-flex">
+                <p className="m-0">To: </p>
+                <RadioMenu
+                  name="group"
+                  options={['All', 'Nationality', 'Role', 'Graduation Date']}
+                  selectedValue={values.group}
+                  onChange={e => {
+                    handleChange(e)
+                    if (e.target.value === 'all') {
+                      const groups = []
+                      setFieldValue('groups', groups)
+                      filterRecipients(recipients, 'all', groups)
+                    } else if (e.target.value === 'nationality') {
+                      const groups = values.nationality
+                      setFieldValue('groups', groups)
+                      filterRecipients(recipients, 'nationality', groups)
+                    } else if (e.target.value === 'role') {
+                      const groups = values.role ? [values.role] : []
+                      setFieldValue('groups', groups)
+                      filterRecipients(recipients, 'role', groups)
+                    } else if (e.target.value === 'graduationDate') {
+                      const groups = isEmpty(values.graduationDate)
+                        ? []
+                        : [values.graduationDate]
+                      setFieldValue('groups', groups)
+                      filterRecipients(recipients, 'graduationDate', groups)
+                    }
+                  }}
+                />
+              </div>
+              <div>Number of Recipients: {values.recipients.length}</div>
+              {values.group === 'nationality' && (
+                <ReactSelect
+                  name="nationality"
+                  placeholder="Select a nationality"
+                  onChange={selectedValue => {
+                    setFieldValue('nationality', selectedValue)
+                    setFieldValue('groups', selectedValue)
+                    filterRecipients(recipients, 'nationality', selectedValue)
+                  }}
+                  value={values.nationality}
+                  multi={true}
+                  options={nationalities}
+                />
+              )}
+              {values.group === 'role' && (
+                <ReactSelect
+                  name="role"
+                  placeholder="Select a role"
+                  onChange={selectedValue => {
+                    if (selectedValue === null) {
+                      selectedValue = undefined
+                      setFieldValue('role', selectedValue)
+                      setFieldValue('groups', [])
+                      filterRecipients(recipients, 'role', [])
+                    } else {
+                      setFieldValue('role', selectedValue)
+                      setFieldValue('groups', [selectedValue])
+                      filterRecipients(recipients, 'role', [selectedValue])
+                    }
+                  }}
+                  value={values.role}
+                  options={[
+                    {
+                      value: { name: 'role', item: 'staff' },
+                      label: 'Staff'
                     },
-                    label: e.target.value
-                  }
-                  setFieldValue('graduationDate', group)
-                  setFieldValue('groups', [group])
-                  filterRecipients(recipients, 'graduationDate', [group])
-                }}
+                    {
+                      value: { name: 'role', item: 'student' },
+                      label: 'Student'
+                    }
+                  ]}
+                />
+              )}
+              {values.group === 'graduationDate' && (
+                <Input
+                  name="graduationDate"
+                  type="date"
+                  value={values.graduationDate.label}
+                  onChange={e => {
+                    const group = {
+                      value: {
+                        name: 'graduationDate',
+                        item: moment
+                          .utc(e.target.value, 'YYYY-MM-DD')
+                          .format('DD/MM/YYYY')
+                      },
+                      label: e.target.value
+                    }
+                    setFieldValue('graduationDate', group)
+                    setFieldValue('groups', [group])
+                    filterRecipients(recipients, 'graduationDate', [group])
+                  }}
+                  onBlur={handleBlur}
+                />
+              )}
+              <Input
+                name="subject"
+                placeholder="Subject"
+                value={values.subject}
+                onChange={handleChange}
                 onBlur={handleBlur}
+                errorMessage={
+                  errors.subject && touched.subject && errors.subject
+                }
               />
-            )}
-            <Input
-              name="subject"
-              placeholder="Subject"
-              value={values.subject}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              errorMessage={errors.subject && touched.subject && errors.subject}
-            />
-            <Editor
-              name="body"
-              wrapperClassName="editorSection"
-              editorClassName="wrapperSection"
-              onContentStateChange={contentState => {
-                setFieldValue('body', contentState)
-                setFieldValue('bodyHtml', draftToHtml(contentState))
-              }}
-              toolbar={{
-                options: ['inline', 'blockType', 'fontSize', 'fontFamily']
-              }}
-            />
-            <div className="formActions">
-              <Link className="formBack" to="/">
-                Back
-              </Link>
-              <Button className="sendButton" text="SEND" />
-            </div>
-          </form>
-        )
-      }}
-    />
-  )
+              <Editor
+                name="body"
+                wrapperClassName="editorSection"
+                editorClassName="wrapperSection"
+                onContentStateChange={contentState => {
+                  setFieldValue('body', contentState)
+                  setFieldValue('bodyHtml', draftToHtml(contentState))
+                }}
+                toolbar={{
+                  options: ['inline', 'blockType', 'fontSize', 'fontFamily']
+                }}
+              />
+              <div className="formActions">
+                <Link className="formBack" to="/">
+                  Back
+                </Link>
+                <Button className="sendButton" text="SEND" />
+              </div>
+            </form>
+          )
+        }}
+      />
+    )
+  } else {
+    return <div>Loading...</div>
+  }
 }
