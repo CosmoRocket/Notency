232ebe5b8ee3ef4df5e2c9bf38481a03def57f74
johnvoon <johnksvoon@gmail.com>
2018-01-19 22:09:49 +1100

    Add loading screen to HomePage component

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -5,6 +5,7 @@ import Notification from '../components/Notification'
 import Announcement from '../components/Announcement'
 import { Link } from 'react-router-dom'
 import Button from '../components/Button'
+import isEmpty from 'lodash/isEmpty'
 
 const HomePage = ({
   notifications,
@@ -13,56 +14,60 @@ const HomePage = ({
   handleLoadMore,
   activeTab
 }) => {
-  const notificationsList = notifications.map(notification => {
-    return (
-      <Notification
-        {...notification}
-        key={notification._id}
-        responses="10/60"
-      />
-    )
-  })
-  const announcementsList = announcements.map(announcement => {
-    return <Announcement {...announcement} key={announcement._id} />
-  })
-  return (
-    <Fragment>
-      <TabbedNav
-        activeTab={activeTab}
-        tabs={[
-          () => <p className="m-0">Emergency Notifications</p>,
-          () => <p className="m-0">General Announcements</p>
-        ]}
-        handleChangeActiveTab={handleChangeActiveTab}
-      />
-      {activeTab === 0 ? (
-        <Link
-          to="/new_notification"
-          className="btn btn-danger text-uppercase font-weight-bold my-2"
-        >
-          New Notification
-        </Link>
-      ) : (
-        <Link
-          to="/new_announcement"
-          className="btn btn-danger text-uppercase font-weight-bold my-2"
-        >
-          New Announcement
-        </Link>
-      )}
-
-      {activeTab === 0 ? notificationsList : announcementsList}
-      {/* Show All Button */}
-      <div className="showAllButton">
-        <Button
-          onClick={() => {
-            handleLoadMore(activeTab)
-          }}
-          text="Show All"
+  if (!isEmpty(notifications) && !isEmpty(announcements)) {
+    const notificationsList = notifications.map(notification => {
+      return (
+        <Notification
+          {...notification}
+          key={notification._id}
+          responses="10/60"
         />
-      </div>
-    </Fragment>
-  )
+      )
+    })
+    const announcementsList = announcements.map(announcement => {
+      return <Announcement {...announcement} key={announcement._id} />
+    })
+    return (
+      <Fragment>
+        <TabbedNav
+          activeTab={activeTab}
+          tabs={[
+            () => <p className="m-0">Emergency Notifications</p>,
+            () => <p className="m-0">General Announcements</p>
+          ]}
+          handleChangeActiveTab={handleChangeActiveTab}
+        />
+        {activeTab === 0 ? (
+          <Link
+            to="/new_notification"
+            className="btn btn-danger text-uppercase font-weight-bold my-2"
+          >
+            New Notification
+          </Link>
+        ) : (
+          <Link
+            to="/new_announcement"
+            className="btn btn-danger text-uppercase font-weight-bold my-2"
+          >
+            New Announcement
+          </Link>
+        )}
+
+        {activeTab === 0 ? notificationsList : announcementsList}
+        {/* Show All Button */}
+        <div className="showAllButton">
+          <Button
+            onClick={() => {
+              handleLoadMore(activeTab)
+            }}
+            text="Show All"
+          />
+        </div>
+      </Fragment>
+    )
+  } else {
+    return <div>Loading notifications and announcements...</div>
+  }
 }
 
 export default HomePage
